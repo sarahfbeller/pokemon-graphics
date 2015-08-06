9b523e7ba9d9768c7ff08faa494a546983d8fea3
Sarah Beller <sarahfbeller@gmail.com>
2015-08-05 20:53:46 -0700

    bone creation done

--- kinematics.cpp
+++ kinematics.cpp
@@ -1,6 +1,7 @@
 #include <GLUT/glut.h>
 #include <math.h>
 #include <vector>
+#include <iostream>
 
 struct Bone {
     float x0, y0, x1, y1;
@@ -15,19 +16,20 @@ float initPos1 = 0.6;
 float initPos2 = 0.3;
 
 void makeBones() {
-    Bone *a;
-    a->x0 = 0.0;
-    a->y0 = 0.0;
-    a->x1 = 0.3;
-    a->y1 = 0.3;
+    bones = std::vector<struct Bone *>();
+    struct Bone *a = new struct Bone;
+    a->x0 = 0.5;
+    a->y0 = 0.5;
+    a->x1 = 0.7;
+    a->y1 = 0.7;
     a->parent = NULL;
     bones.push_back(a);
-    Bone *b;
-    b->x0 = 0.5;
-    b->y0 = 0.5;
-    b->x1 = 0.7;
-    b->y1 = 0.7;
-    a->parent = a;
+    struct Bone *b = new struct Bone;
+    b->x0 = 0.0;
+    b->y0 = 0.0;
+    b->x1 = 0.3;
+    b->y1 = 0.3;
+    b->parent = a;
     bones.push_back(b);
 }
 
@@ -54,11 +56,19 @@ void display() {
     //     glVertex2f(initPos1, initPos1);
     //     glVertex2f(initPos2, initPos2);
     // glEnd();
+
     glBegin(GL_LINES);
-        glVertex2f();
+        for(int i = 0; i < bones.size(); i++) {
+            struct Bone *bone = bones.at(i);
+            if (!bone->parent) {
+                glVertex2f(bone->x0, bone->y0);
+                glVertex2f(bone->x1, bone->y1);
+            } else {
+                glVertex2f(bone->parent->x0, bone->parent->y0);
+                glVertex2f(bone->x1, bone->y1);
+            }
+        }
     glEnd();
-
-
     glFlush();
     // glutPostRedisplay();
 }
@@ -71,13 +81,13 @@ void init() {
 }
 
 int main(int argc, char** argv) {
+    makeBones();
 	glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500, 500); 
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Kinematics");
     init();
-    // makeBones();
     glutDisplayFunc(display); 
     glutMainLoop();
     return 0;
