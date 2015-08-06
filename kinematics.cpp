0d1a1d5b1122b5e9f4bfe1d48cdb07db891fc6cd
Sarah Beller <sarahfbeller@gmail.com>
2015-08-06 16:14:08 -0700

    made 5 hand bones that move as 1 hand on wrist joint

--- kinematics.cpp
+++ kinematics.cpp
@@ -19,12 +19,17 @@ void makeBones() {
     struct Bone *a = new struct Bone;
     struct Bone *b = new struct Bone;
     struct Bone *c = new struct Bone;
+    struct Bone *d = new struct Bone;
+    struct Bone *e = new struct Bone;
+    struct Bone *f = new struct Bone;
+    struct Bone *g = new struct Bone;
+
     a->x0 = 0.3;
     a->y0 = 0.3;
     a->angle = 20.0;
     a->minAngle = -90.0;
     a->maxAngle = 90.0;
-    a->len = 0.2;
+    a->len = 0.35;
     a->children[0] = b;
     a->numChildren++;
 
@@ -33,19 +38,52 @@ void makeBones() {
     b->angle = 40.0;
     b->minAngle = 0.0;
     b->maxAngle = 135.0;
-    b->len = 0.2;
+    b->len = 0.3;
     b->children[0] = c;
-    b->numChildren++;
+    b->children[1] = d;
+    b->children[2] = e;
+    b->children[3] = f;
+    b->children[4] = g;
+    b->numChildren += 5;
 
     c->x0 = 0.0;
     c->y0 = 0.0;
     c->angle = 10.0;
-    c->minAngle = -10.0;
-    c->maxAngle = 10.0;
+    c->minAngle = 0.0;
+    c->maxAngle = 20.0;
     c->len = 0.05;
+    d->x0 = 0.0;
+    d->y0 = 0.0;
+    d->angle = 5.0;
+    d->minAngle = -5.0;
+    d->maxAngle = 15.0;
+    d->len = 0.05;
+    e->x0 = 0.0;
+    e->y0 = 0.0;
+    e->angle = 0.0;
+    e->minAngle = -10.0;
+    e->maxAngle = 10.0;
+    e->len = 0.05;
+    f->x0 = 0.0;
+    f->y0 = 0.0;
+    f->angle = -5.0;
+    f->minAngle = -15.0;
+    f->maxAngle = 5.0;
+    f->len = 0.05;
+    g->x0 = 0.0;
+    g->y0 = 0.0;
+    g->angle = -10.0;
+    g->minAngle = -20.0;
+    g->maxAngle = 0.0;
+    g->len = 0.05;
+
     bones.push_back(a);
     bones.push_back(b);
     bones.push_back(c);
+    bones.push_back(d);
+    bones.push_back(e);
+    bones.push_back(f);
+    bones.push_back(g);
 }
 
 void drawBone(struct Bone *bone, bool isRoot) {
@@ -114,18 +152,22 @@ void keyFunc(unsigned char key, int x, int y) {
         }
         glutPostRedisplay();      
     } else if (key == 'u') { // rotate hand anticlockwise
-        struct Bone * bone = bones[2];
-        float newAngle = bone->angle + rotDelta;
-        if (newAngle >= bone->minAngle && newAngle <= bone->maxAngle) {
-            bone->angle = newAngle;
-        } 
+        for(int i = 2; i < 7; i++) {
+            struct Bone * bone = bones[i];
+            float newAngle = bone->angle + rotDelta;
+            if (newAngle >= bone->minAngle && newAngle <= bone->maxAngle) {
+                bone->angle = newAngle;
+            }
+        }
         glutPostRedisplay();       
     } else if (key == 'i') { // rotate hand clockwise
-        struct Bone * bone = bones[2];
-        float newAngle = bone->angle - rotDelta;
-        if (newAngle >= bone->minAngle && newAngle <= bone->maxAngle) {
-            bone->angle = newAngle;
-        }  
+        for(int i = 2; i < 7; i++) {
+            struct Bone * bone = bones[i];
+            float newAngle = bone->angle - rotDelta;
+            if (newAngle >= bone->minAngle && newAngle <= bone->maxAngle) {
+                bone->angle = newAngle;
+            }
+        } 
         glutPostRedisplay();      
     }
 }
