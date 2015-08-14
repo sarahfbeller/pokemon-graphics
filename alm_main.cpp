1dd8802ec9a92341e93f611ddf11bc4f3b119220
Sarah Beller <sarahfbeller@gmail.com>
2015-08-13 21:19:20 -0700

    same

--- alm_main.cpp
+++ alm_main.cpp
@@ -264,7 +264,7 @@ void drawCharacter(){
     		        // std::cout<<o_vec[0]<<" "<<o_vec[1]<<" "<<o_vec[2]<<std::endl;
               //       std::cout<<new_coords[0]<<" "<<new_coords[1]<<" "<<new_coords[2]<<std::endl;
               //   }
-        		glVertex3f(new_coords[0] + b->x0, new_coords[1] + b->y0, new_coords[2] + b->z0);
+        		glVertex3f(new_coords[0] + b->x0 + 0, new_coords[1] + b->y0, new_coords[2] + b->z0);
             }
         glEnd();
     }
